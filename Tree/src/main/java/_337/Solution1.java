647406b634578532d3981f37eb60e2c7877ae092
huangbxaaa <huangbx666@gmail.com>
2020-11-07 21:30:22 +0800

    11.7

---
+++ Tree/src/main/java/_337/Solution1.java
@@ -0,0 +1,49 @@
+package _337;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+public class Solution1 {
+    public int rob(TreeNode root) {
+        return Math.max(pickOrNot(root, true), pickOrNot(root, false));
+    }
+
+    public int pickOrNot(TreeNode node, boolean isPick) {
+        if (node == null) {
+            return 0;
+        }
+        if (isPick) {
+            return Math.max(node.val + pickOrNot(node.left, false) + pickOrNot(node.right, false),
+                    pickOrNot(node.left, true) + pickOrNot(node.right, true));
+        } else {
+          return pickOrNot(node.left, true) + pickOrNot(node.right, true);
+        }
+
+    }
+
+
+    @Test
+    public void test1() {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(3);
+        root.right.right = new TreeNode(1);
+        int rob = rob(root);
+        System.out.println(rob);
+    }
+
+
+    @Test
+    public void test2() {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.right = new TreeNode(1);
+        int rob = rob(root);
+        System.out.println(rob);
+    }
+}
---
+++ Tree/src/main/java/_337/TreeNode.java
@@ -0,0 +1,11 @@
+package _337;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
